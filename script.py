5c7b5b07d3fd596cc7813199492fd613d8c0177a
borhen eddine dahech <bdahech08@gmail.com>
2023-11-15 13:59:07 +0100

    update

--- script.py
+++ script.py
@@ -4,7 +4,8 @@ from selenium.webdriver.edge.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import pandas as pd
-
+from dotenv import load_dotenv
+import os
 import time
 
 
@@ -16,6 +17,7 @@ driver = webdriver.Edge(service=service)
 
 driver.get("https://soccerguru.live/dashboard")
 time.sleep(10)
+
 try:
     container = driver.find_element(By.XPATH,'//*[@id="app-mount"]/div[2]/div[1]/div[1]/div/div/div/div/form')
 except:
@@ -23,9 +25,9 @@ except:
 try:
 
     email_box = container.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[1]/div/div/div/div/form/div[2]/div/div[1]/div[2]/div[1]/div/div[2]/input')
-    email_box.send_keys("bdahech08@gmail.com")
+    email_box.send_keys(os.getenv("login"))
     mdp_box = container.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[1]/div/div/div/div/form/div[2]/div/div[1]/div[2]/div[2]/div/input')
-    mdp_box.send_keys("borhen69!")
+    mdp_box.send_keys(os.getenv("password"))
     email_box.submit()
 
 except:
