1068265560a54f34b11e2c0b1fa3101b87b9bf03
borhen eddine dahech <bdahech08@gmail.com>
2023-11-19 11:50:34 +0100

    Update script.py

--- script.py
+++ script.py
@@ -18,79 +18,6 @@ service = Service(PATH)
 
 driver = webdriver.Edge(service=service)
 
-# #login 
-# login=os.getenv("login")
-# password=os.getenv("password")
-
-# driver.get("https://soccerguru.live/dashboard")
-
-# try:
-#     container = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located((By.XPATH, '//*[@id="app-mount"]/div[2]/div[1]/div[1]/div/div/div/div/form'))
-#     )
-# except:
-#     print("container not found")
-# try:
-#     email_box = container.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[1]/div/div/div/div/form/div[2]/div/div[1]/div[2]/div[1]/div/div[2]/input')
-#     email_box.send_keys(login)
-#     mdp_box = container.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[1]/div/div/div/div/form/div[2]/div/div[1]/div[2]/div[2]/div/input')
-#     mdp_box.send_keys(password)
-#     email_box.submit()
-#     time.sleep(7)
-
-# except:
-#     print("inputs not found")
-
-# try:
-#     button_container = WebDriverWait(driver, 10).until(
-#     EC.presence_of_element_located((By.CLASS_NAME,'authorize__4ddfc'))
-#     )
-# except:
-#     print("container not find")
-# try:
-#     button = button_container.find_element(By.CSS_SELECTOR,'#app-mount > div.appAsidePanelWrapper__714a6 > div.notAppAsidePanel__9d124 > div.app_b1f720 > div > div > div > div > div > div.footer__04ee2 > button.button_afdfd9.lookFilled__19298.colorBrand_b2253e.sizeMedium_c6fa98.grow__4c8a4')
-#     button.click()
-# except:
-#     print("button not found")
-
-# time.sleep(5)
-
-
-# actions = ActionChains(driver)
-
-# #claiming ad reward
-# try:
-#     watch_ad = driver.find_element(By.CSS_SELECTOR,"#__nuxt > div > div.flex-grow > div > div > div.flex.flex-col.gap-4.px-4.md\:px-8 > div.flex.flex-col.gap-4.bg-blue-600.p-8.rounded.shadow > a")
-#     actions.move_to_element(watch_ad).perform()
-#     actions.click().perform()
-#     time.sleep(30)
-#     driver.quit
-# except:
-#     print("watch_ad not found")
-
-# #claiming claim reward
-# try:
-#     claim= driver.find_element(By.CSS_SELECTOR,"#__nuxt > div > div.flex-grow > div > div > div.flex.flex-col.gap-4.px-4.md\:px-8 > div:nth-child(3) > div:nth-child(1) > a")
-#     actions.move_to_element(claim).perform()
-#     actions.click().perform()
-#     time.sleep(5)
-# except:
-#     print("claim not found")
-
-
-# #claiming daily reward
-# try:
-#     daily= driver.find_element(By.CSS_SELECTOR,"#__nuxt > div > div.flex-grow > div > div > div.flex.flex-col.gap-4.px-4.md\:px-8 > div:nth-child(3) > div:nth-child(2) > a")
-#     actions.move_to_element(daily).perform()
-#     actions.click().perform()
-#     time.sleep(5)
-# except:
-#     print("daily not found")
-
-
-
-# time.sleep(5)
-
 import os
 import time
 from selenium import webdriver
@@ -150,7 +77,7 @@ def claim_reward(driver):
 
 def main():
     driver = webdriver.Edge()  # Make sure to replace with the appropriate WebDriver
-    login(driver, os.getenv("login"), os.getenv("password"))
+    login(driver, os.getenv("login"), os.getenv("password")) #Make sure to put your own login and password of discord
 
     claim_reward(driver)
 
